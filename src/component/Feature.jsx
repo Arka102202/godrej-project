25a6c72433afc6310ffdf499918b30b92230fe33
Arka102202 <arka.online1995@gmail.com>
2024-05-03 07:25:36 +0530

    features is resposive now

--- src/component/Feature.jsx
+++ src/component/Feature.jsx
@@ -13,10 +13,10 @@ const Feature = () => {
       </div>
 
       <div className="section">
-        <div className='left left--1'>
+        <div className='img img--1'>
           <p className='img-desc'>Fitness Center</p>
         </div>
-        <div className='right--1'>
+        <div className='text text--1'>
           <div className="heading-box">
             <p className='heading'>COMMUNITY</p>
             <p className='heading'>COURTYARD</p>
@@ -28,34 +28,50 @@ const Feature = () => {
           <ArrowBox />
         </div>
       </div>
-      <div className="section">
-        <div className='left--3'>
+      <div className="section section--1">
+        <div className='text text--2'>
           <div className="heading-box">
-            <p className='heading'>COMMUNITY</p>
-            <p className='heading'>COURTYARD</p>
-            <p className='sub-heading'>AT THE HEART OF A LIFE WITH ACTIVE EXPERIENCES THAT MAKE LIFE EXCITING</p>
+            <p className='heading'>SPORTS</p>
+            <p className='heading'>ARCADE</p>
+            <p className='sub-heading'>PURSUE YOUR FAVOURITE SPORTS</p>
           </div>
           <p className='body'>
-            Treat yourself to the comfort of community living as you pursue your favourite things at the Podium Level. Here, there is something for everyone to enjoy.
+            Embrace an active lifestyle that helps you find the much - needed balance between life and work with ease.Play your favourite sport with renewed passion every day.
           </p>
           <ArrowBox />
         </div>
-        <div className='right--3'>
+        <div className='img img--2'>
           <p className='img-desc'>Badminton Court</p>
         </div>
       </div>
-      <div className="section">
-        <div className='left left--2'>
-          <p className='img-desc'>Lounge In a Sky Sanctuary</p>
+      <div className="section section--2">
+        <div className='img img--2'>
+          <p className='img-desc'>Badminton Court</p>
         </div>
-        <div className='right'>
+        <div className='text text--2'>
           <div className="heading-box">
-            <p className='heading'>COMMUNITY</p>
-            <p className='heading'>COURTYARD</p>
-            <p className='sub-heading'>AT THE HEART OF A LIFE WITH ACTIVE EXPERIENCES THAT MAKE LIFE EXCITING</p>
+            <p className='heading'>SPORTS</p>
+            <p className='heading'>ARCADE</p>
+            <p className='sub-heading'>PURSUE YOUR FAVOURITE SPORTS</p>
           </div>
           <p className='body'>
-            Treat yourself to the comfort of community living as you pursue your favourite things at the Podium Level. Here, there is something for everyone to enjoy.
+            Embrace an active lifestyle that helps you find the much - needed balance between life and work with ease.Play your favourite sport with renewed passion every day.
+          </p>
+          <ArrowBox />
+        </div>
+      </div>
+      <div className="section">
+        <div className='img img--3'>
+          <p className='img-desc'>Lounge In a Sky Sanctuary</p>
+        </div>
+        <div className='text text--3'>
+          <div className="heading-box">
+            <p className='heading'>PANORAMIC</p>
+            <p className='heading'>PORTAL</p>
+            <p className='sub-heading'>SURROUND YOURSELF WITH SKY-HIGH LUXURIES IN THE HEART OF MUMBAI</p>
+          </div>
+          <p className='body'>
+            Soak in stellar views in the lap of the sky, as you spend time doing things that bring you infinite joy.
           </p>
           <ArrowBox />
         </div>
@@ -78,5 +94,9 @@ const ArrowBox = () => {
     <p className='count'>01/10</p>
   </div>
 }
+
+
+
+
 
 
